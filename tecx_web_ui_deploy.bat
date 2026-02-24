521a95105e3052cca739bb02c9819c0364a666ab
TecXCTO <cto.tecx@gmail.com>
2026-02-24 08:49:30 +0530

    Update tecx_web_ui_deploy.bat

--- tecx_web_ui_deploy.bat
+++ tecx_web_ui_deploy.bat
@@ -1,4 +1,4 @@
 # Build the Image:
-docker build -t resonance-science-ai .
+docker build -t tecx-science-ai .
 # Run on your GPU:
-docker run --gpus all -p 7860:7860 resonance-science-ai
+docker run --gpus all -p 7860:7860 tecx-science-ai
